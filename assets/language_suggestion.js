cdf044adddf23f8b678aa3e2bba049df27692c79
Minnur Yunusov <mmyunusov@gmail.com>
2019-11-01 21:49:05 -0700

    Cleanup.

--- assets/language_suggestion.js
+++ assets/language_suggestion.js
@@ -3,26 +3,29 @@
   'use strict';
 
   $(document).ready(function() {
+
     var current_lang = drupalSettings.language_suggestion.current_language.toLowerCase();
-    var pathLangCode = drupalSettings.path.currentLanguage;
     var settings = drupalSettings.language_suggestion.settings;
-    if (settings.language_detection == 'browser') {
-      // Browser-based redirection
-      var browser_lang = window.navigator.userLanguage || window.navigator.language; // Set to 'de' for debugging.
-      languageSuggestionRoutine();
-    }
-    else {
-      // HTTP header-based redirection
-      var browser_lang = '';
-      var xhr = $.ajax({
-        type: "GET",
-        url: '/language-suggestion/get-header-parameter',
-        success: function(output, status) { 
-          browser_lang = output;
-          languageSuggestionRoutine();
-        },
-        error: function(output) {}
-      });
+
+    if (settings.enabled) {
+      if (settings.language_detection == 'browser') {
+        // Browser-based redirection
+        var browser_lang = window.navigator.userLanguage || window.navigator.language; // Set to 'de' for debugging.
+        languageSuggestionRoutine();
+      }
+      else {
+        // HTTP header-based redirection
+        var browser_lang = '';
+        var xhr = $.ajax({
+          type: "GET",
+          url: '/language-suggestion/get-header-parameter',
+          success: function(output, status) { 
+            browser_lang = output;
+            languageSuggestionRoutine();
+          },
+          error: function(output) {}
+        });
+      }
     }
 
     function languageSuggestionRoutine() {
@@ -43,51 +46,48 @@
       var date = new Date();
       var timestamp = date.getTime();
 
-      if (settings.enabled) {
+      // This code disables auto redirect when a visitor decides to switch languages in the UI.
+      if (settings.disable_redirect_class) {
+        $(settings.disable_redirect_class).on('click', function(e) {
+          $.removeCookie(settings.cookie_prefix + '.always_redirect');
+          $.removeCookie(settings.cookie_prefix + '.redirect_lang');
+        });
+      }
 
-        // This code disables auto redirect when a visitor decides to switch languages in the UI.
-        if (settings.disable_redirect_class) {
-          $(settings.disable_redirect_class).on('click', function(e) {
-            $.removeCookie(settings.cookie_prefix + '.always_redirect');
-            $.removeCookie(settings.cookie_prefix + '.redirect_lang');
-          });
-        }
+      // Auto redirect to previously selected langauge.
+      // Also making sure we are not creating a redirect loop when already switch to a language.
+      if (settings.always_redirect && redrectLangCode !== undefined && redrectLangCode !== current_lang) {
+        window.location.href = redrectLangCode;
+      }
 
-        // Auto redirect to previously selected langauge.
-        // Also making sure we are not creating a redirect loop when already switch to a language.
-        if (settings.always_redirect && redrectLangCode !== undefined && redrectLangCode !== current_lang) {
-          window.location.href = redrectLangCode;
-        }
-
-        // Looping through available language mapping to find if any mapped languages matching to a visitors browser language.
-        for (var langcode in settings.mapping) {
-          if (settings.mapping.hasOwnProperty(langcode)) {
-            var langObject = settings.mapping[langcode];
-            var codeArr = langObject.browser_lang.split(',');
-            var index = 0;
-            while (index < codeArr.length) { 
-              if (codeArr[index] != current_lang && codeArr[index] == browser_lang.toLowerCase()) {
-                show_suggestion = true;
-                lang_code = langcode;
-                continue_link = langObject.continue_link;
-                message = langObject.message;
-                url = langObject.url;
-                custom_url = langObject.custom_url;
-                default_url = langObject.default_url;
-              }
-              index++; 
+      // Looping through available language mapping to find if any mapped languages matching to a visitors browser language.
+      for (var langcode in settings.mapping) {
+        if (settings.mapping.hasOwnProperty(langcode)) {
+          var langObject = settings.mapping[langcode];
+          var codeArr = langObject.browser_lang.split(',');
+          var index = 0;
+          while (index < codeArr.length) { 
+            if (codeArr[index] != current_lang && codeArr[index] == browser_lang.toLowerCase()) {
+              show_suggestion = true;
+              lang_code = langcode;
+              continue_link = langObject.continue_link;
+              message = langObject.message;
+              url = langObject.url;
+              custom_url = langObject.custom_url;
+              default_url = langObject.default_url;
             }
+            index++; 
           }
         }
+      }
 
-        // Show the language suggestion box. Make sure user hasn't dismissed in the past or dismiss hasn't expired yet.
-        if (show_suggestion && message && (dismissed <= timestamp || dismissed === undefined)) {
-          layoutContainer.append('<div id="language-suggestion"<div class="ls-wrapper"><div class="ls-message">'
-            + message + '</div><div class="ls-goto"><a href="#" id="ls-continue">' + ((continue_link !== undefined) ? continue_link : Drupal.t('Continue'))
-            + '</a></div><div class="ls-dismiss"><a href="#" id="ls-dismiss">'
-            + Drupal.t('Dismiss') + '</a></div></div></div>');
-          layoutContainer.find('#language-suggestion').delay(settings.show_delay * 1000).show('slow');
-        }
+      // Show the language suggestion box. Make sure user hasn't dismissed in the past or dismiss hasn't expired yet.
+      if (show_suggestion && message && (dismissed <= timestamp || dismissed === undefined)) {
+        layoutContainer.append('<div id="language-suggestion"<div class="ls-wrapper"><div class="ls-message">'
+          + message + '</div><div class="ls-goto"><a href="#" id="ls-continue">' + ((continue_link !== undefined) ? continue_link : Drupal.t('Continue'))
+          + '</a></div><div class="ls-dismiss"><a href="#" id="ls-dismiss">'
+          + Drupal.t('Dismiss') + '</a></div></div></div>');
+        layoutContainer.find('#language-suggestion').delay(settings.show_delay * 1000).show('slow');
       }
 
       // Continue to the language suggested and make sure we add to autoredirect cookie if such option is enabled in the module settings.
